2c46555abaf2a7224685a887f48cecc755ab1537
Pavel Prigoda <rotares@disroot.org>
2026-03-05 19:30:03 +0300

    refactor(components): Divide App.jsx into components

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,14 @@
+import Button from "./Button"
+import Field from "./Field"
+
+//форма добавления задачи
+function AddTaskForm() {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  )
+}
+
+export default AddTaskForm
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <button className="button" type="submit">
+      Add
+    </button>
+  )
+}
+
+export default Button
