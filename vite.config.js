9cd6b37aafd996fb4830c172f636a4255b1878ca
Pavel Prigoda <rotares@disroot.org>
2026-03-21 17:35:01 +0300

    build(vite config, dep): add dep for deploy, change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
 import path from 'path'
+import { defineConfig } from 'vite'
 
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+
+  return {
+    base: isProd ? '/react-todo/' : '/',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  }
 })
