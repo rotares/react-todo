649045e8835729be37762633472ca33eff37a875
Pavel Prigoda <rotares@disroot.org>
2026-03-25 17:08:27 +0300

    refactor(TodoInfo):  add props

--- src/components/TodoInfo.jsx
+++ src/components/TodoInfo.jsx
@@ -1,12 +1,18 @@
+import { useTasksContext } from "../context/TasksContext"
+import { Button } from "./Button"
+
 export const TodoInfo = () => {
+  const { tasks } = useTasksContext()
+  const totalTasks = tasks.length
+
   return (
     <div className="todo__info">
-      <div className="todo__total-tasks">
-        Total tasks: <span>0</span>
-      </div>
-      <button className="todo__delete-all-button" type="button">
-        Delete all
-      </button>
+      <div className="todo__total-tasks">Total tasks: {totalTasks}</div>
+      {tasks.length > 0 && (
+        <Button type="button" className="todo__delete-all-button">
+          Delete all
+        </Button>
+      )}
     </div>
   )
 }
