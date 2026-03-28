ae880ae02886282cc8873f227a1fddd4cc8cbac2
Pavel Prigoda <rotares@disroot.org>
2026-03-28 18:31:00 +0300

    feat(TodoItem, routes): add styles, function for return 404 page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,12 @@
 import TaskPage from "@/pages/TaskPage"
 import TasksPage from "@/pages/TasksPage"
-import { Router } from "./routing/Router"
+import { Router } from "./routing"
 
 const App = () => {
   const routes = {
     "/": TasksPage,
     "/tasks/:id": TaskPage,
-    "*": <div>4024 Not Found</div>,
+    "*": () => <div>404 Not Found</div>,
   }
   return <Router routes={routes}></Router>
 }
