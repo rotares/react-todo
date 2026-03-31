5ec9e6501d310145e11310a1a298cfc60acf25dd
Pavel Prigoda <rotares@disroot.org>
2026-03-31 04:43:15 +0300

    refactor(Button): migrate to typescript

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react"
+import styles from "./Button.module.css"
+
+type ButtonType = "button" | "submit" | "reset"
+
+type ButtonProps = {
+  type?: ButtonType
+  children?: ReactNode
+  onClick?: () => void
+  isDisabled: boolean
+} & Record<string | number, unknown>
+
+export const Button = (props: ButtonProps) => {
+  const { type = "submit", children, onClick, isDisabled, ...rest } = props
+  console.log(props, "props")
+  console.log(rest, "rest")
+  return (
+    <button
+      className={` ${styles.button}`}
+      disabled={isDisabled}
+      type={type}
+      onClick={onClick}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
