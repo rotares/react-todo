5a7b9586c4928be8ef554c36846891d30832c563
Pavel Prigoda <rotares@disroot.org>
2026-03-31 05:13:42 +0300

    refactor(Field): migrate to typescript

---
+++ src/shared/ui/Field/Field.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps } from "react"
+import styles from "./Field.module.css"
+
+//включает в себя ComponentProps и кастомные поля
+interface FieldProps extends ComponentProps<"input"> {
+  className?: string
+  label: string
+  id: string
+  onInput?: () => void
+}
+
+export const Field = (props: FieldProps) => {
+  const { className = "", value, label, id, onInput, ...rest } = props
+
+  return (
+    <div className={`${className} ${styles.field}`}>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        value={value}
+        className={styles.input}
+        id={id}
+        onInput={onInput}
+        placeholder=" "
+        autoComplete="off"
+        {...rest}
+      />
+    </div>
+  )
+}
