4c7aa851e9af6f04753ef67a4b96b78d1822d0ab
Pavel Prigoda <rotares@disroot.org>
2026-04-03 18:53:47 +0300

    feat(vite config): add dynamic base

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "build" ? "/react-todo" : "/",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  }
 })
